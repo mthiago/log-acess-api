c43ca0b7df7d980d6e850dee951b5a2e40da7417
Thiago Ávila <mthiago>
2019-10-10 22:21:50 -0300

    rota post

--- src/com/rest/resources/LogAcessEndPoint.java
+++ src/com/rest/resources/LogAcessEndPoint.java
@@ -1,16 +1,23 @@
 package com.rest.resources;
 
-import javax.ws.rs.GET;
+import javax.ws.rs.*;
 import javax.ws.rs.Path;
+import javax.ws.rs.core.MediaType;
+
+import request.LogRequest;
 
 @Path("/log-acess")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class LogAcessEndPoint {
 
-	@Path("/getData")
-	@GET
-	public String getData() {
+	@POST
+	@Path("/laar/ingest")
+	@Produces("application/json;charset=utf-8")
+	@Consumes("application/json")
+	public String getData(LogRequest request) {
 		
-		return "a";
+		return request.getUrl();
 	}
 
 }
---
+++ src/request/LogRequest.java
@@ -0,0 +1,38 @@
+package request;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement
+public class LogRequest {
+
+	private String url;
+	private Long timestamp;
+	private String userId;
+	private Integer region;
+
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Long getTimestamp() {
+		return timestamp;
+	}
+	public void setTimestamp(Long timestamp) {
+		this.timestamp = timestamp;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public Integer getRegion() {
+		return region;
+	}
+	public void setRegion(Integer region) {
+		this.region = region;
+	}
+
+}
