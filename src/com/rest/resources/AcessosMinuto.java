c9e75dc07fecd756e67fc86c299b047fb065e234
Thiago Ávila <mthiago>
2019-10-12 01:31:35 -0300

    metricas terminadas

---
+++ src/com/rest/resources/AcessosMinuto.java
@@ -0,0 +1,22 @@
+package com.rest.resources;
+
+public class AcessosMinuto {
+
+	private Integer minuto;
+	private Integer quantidade;
+	public Integer getMinuto() {
+		return minuto;
+	}
+	public void setMinuto(Integer minuto) {
+		this.minuto = minuto;
+	}
+	public Integer getQuantidade() {
+		return quantidade;
+	}
+	public void setQuantidade(Integer quantidade) {
+		this.quantidade = quantidade;
+	}
+	
+	
+	
+}
---
+++ src/com/rest/resources/AllTimestamp.java
@@ -0,0 +1,21 @@
+package com.rest.resources;
+
+import java.util.List;
+
+public class AllTimestamp {
+	
+	private List<Long> timestamp;
+
+	public List<Long> getTimestamp() {
+		return timestamp;
+	}
+
+	public void setTimestamp(List<Long> timestamp) {
+		this.timestamp = timestamp;
+	}
+
+
+	
+	
+
+}
