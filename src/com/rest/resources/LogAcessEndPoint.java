f6db8cede36c332180d058a111912c77412f736f
Thiago Ávila <mthiago>
2019-10-11 22:04:22 -0300

    a

--- src/com/rest/resources/LogAcessEndPoint.java
+++ src/com/rest/resources/LogAcessEndPoint.java
@@ -27,23 +27,68 @@ public class LogAcessEndPoint {
 	@Produces("application/json;charset=utf-8")
 	@Consumes("application/json")
 	public String metrics() {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
+		
+		
+		
 		try {
 			
 			Connection connection = DatabaseConnection.getConexaoMySQL();
-           String query = "select count(*) from logs where url like '%/pets/exotic/cats/10%'";
+			String primeiraMetrica = consultaPrimeiraMetrica(connection);
+          
+	       return primeiraMetrica;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
 
+	@POST
+	@Path("/laar/ingest")
+	@Produces("application/json;charset=utf-8")
+	@Consumes("application/json")
+	public Boolean salvarLog(LogRequest request) {
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			Connection connection = DatabaseConnection.getConexaoMySQL();
 
-           //PEGANDO CONTAGEM DE VISITANTES
-           PreparedStatement Stmt = connection.prepareStatement(query);
+			// the mysql insert statement
+			String query = "INSERT INTO LOGS(url, timestamp, userId, region) VALUES(?, ?, ?, ?)";
 
-           rs = Stmt.executeQuery();
-           rs.next();
-           String metricCats = rs.getString(1);
-           Integer catsInteger = Integer.valueOf(metricCats);
-           
-          Stmt.close();
+			// create the mysql insert preparedstatement
+			pstmt = connection.prepareStatement(query);
+
+			pstmt.setString(1, request.getUrl());
+			pstmt.setLong(2, request.getTimestamp());
+			pstmt.setString(3, request.getUserId());
+			pstmt.setInt(4, request.getRegion());
+			pstmt.executeUpdate();
+			pstmt.close();
+
+			return true;
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return false;
+		}
+	}
+	
+	public String consultaPrimeiraMetrica(Connection connection){
+		String msgReturn = "";
+		try {
+        String query = "select count(*) from logs where url like '%/pets/exotic/cats/10%'";
+
+        
+        PreparedStatement pstmt = null;
+		ResultSet rs = null;
+        //PEGANDO CONTAGEM DE VISITANTES
+        PreparedStatement Stmt = connection.prepareStatement(query);
+
+        rs = Stmt.executeQuery();
+        rs.next();
+        String metricCats = rs.getString(1);
+        Integer catsInteger = Integer.valueOf(metricCats);
+        
+       Stmt.close();
 	        
 	        
 	           query = "select count(*) from logs where url like '%/pets/guaipeca/dogs/1%'";
@@ -99,52 +144,18 @@ public class LogAcessEndPoint {
 	          StringBuilder msg = new StringBuilder();
 	      for (Metric1Response r : ordenada) {
 	    	  if (r.getPosition() != null && r.getUrl() != null) {
-	    		  infos = "A url mais acessada no mundo é" + r.getUrl() + "e ela teve " + r.getPosition() + " acessos";
+	    		  infos = "A url mais acessada no mundo é " + r.getUrl() + " e ela teve " + r.getPosition() + " acessos\n";
 	    		  msg.append(infos);
 	    	  }
 	      }
 	      
-	      String msgReturn = msg.toString();
-          
+	      msgReturn = msg.toString();
+		}
+		catch (Exception e) {
+			
+		}
 	      
 	      return msgReturn;
-          
-          
-	       
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
-
-	@POST
-	@Path("/laar/ingest")
-	@Produces("application/json;charset=utf-8")
-	@Consumes("application/json")
-	public Boolean salvarLog(LogRequest request) {
-		PreparedStatement pstmt = null;
-		ResultSet rs = null;
-		try {
-			Connection connection = DatabaseConnection.getConexaoMySQL();
-
-			// the mysql insert statement
-			String query = "INSERT INTO LOGS(url, timestamp, userId, region) VALUES(?, ?, ?, ?)";
-
-			// create the mysql insert preparedstatement
-			pstmt = connection.prepareStatement(query);
-
-			pstmt.setString(1, request.getUrl());
-			pstmt.setLong(2, request.getTimestamp());
-			pstmt.setString(3, request.getUserId());
-			pstmt.setInt(4, request.getRegion());
-			pstmt.executeUpdate();
-			pstmt.close();
-
-			return true;
-		} catch (SQLException e) {
-			e.printStackTrace();
-			return false;
-		}
 	}
 
 }
