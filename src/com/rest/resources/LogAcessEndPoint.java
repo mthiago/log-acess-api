2224ff039d00262902c4fcb5ee1f077cd4292143
Thiago Ávila <mthiago>
2019-10-11 21:57:46 -0300

    1 metrica: retornando info

--- src/com/rest/resources/LogAcessEndPoint.java
+++ src/com/rest/resources/LogAcessEndPoint.java
@@ -6,7 +6,10 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.ws.rs.*;
 import javax.ws.rs.Path;
@@ -23,7 +26,7 @@ public class LogAcessEndPoint {
 	@Path("/laar/metrics")
 	@Produces("application/json;charset=utf-8")
 	@Consumes("application/json")
-	public Boolean metrics() {
+	public String metrics() {
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
 		try {
@@ -38,6 +41,7 @@ public class LogAcessEndPoint {
            rs = Stmt.executeQuery();
            rs.next();
            String metricCats = rs.getString(1);
+           Integer catsInteger = Integer.valueOf(metricCats);
            
           Stmt.close();
 	        
@@ -51,14 +55,64 @@ public class LogAcessEndPoint {
 	           rs = Stmt.executeQuery();
 	           rs.next();
 	           String metricDogs = rs.getString(1);
+	           Integer dogsInteger = Integer.valueOf(metricDogs);
+	           
+	           
+	           Stmt.close();
+		        
+		        
+	           query = "select count(*) from logs where url like '%/tiggers/bid/now%'";
+
+
+	           //PEGANDO CONTAGEM DE VISITANTES
+	           Stmt = connection.prepareStatement(query);
+
+	           rs = Stmt.executeQuery();
+	           rs.next();
+	           String metricBid = rs.getString(1);
+	           Integer bidInteger = Integer.valueOf(metricBid);
+	           
+	           
+	          List<Metric1Response> response = new ArrayList<Metric1Response>();
+	           
+	          Metric1Response cat = new Metric1Response();
+	          cat.setPosition(catsInteger);
+	          cat.setUrl("/pets/exotic/cats/10");
+	          response.add(cat);
+	          
+	          
+	          Metric1Response dog = new Metric1Response();
+	          dog.setPosition(dogsInteger);
+	          dog.setUrl("/pets/guaipeca/dogs/1");
+	          response.add(dog);
+	          
+	          Metric1Response bid = new Metric1Response();
+	          bid.setPosition(bidInteger);
+	          cat.setUrl("/tiggers/bid/now");
+	          response.add(cat);
+
+	          List<Metric1Response> ordenada = response.stream()
+	        		    .sorted(Comparator.comparing(Metric1Response::getPosition).reversed())
+	        		    .collect(Collectors.toList()); 
+	          
+	          String infos = "";
+	          StringBuilder msg = new StringBuilder();
+	      for (Metric1Response r : ordenada) {
+	    	  if (r.getPosition() != null && r.getUrl() != null) {
+	    		  infos = "A url mais acessada no mundo é" + r.getUrl() + "e ela teve " + r.getPosition() + " acessos";
+	    		  msg.append(infos);
+	    	  }
+	      }
+	      
+	      String msgReturn = msg.toString();
           
-          
+	      
+	      return msgReturn;
           
           
 	       
 		} catch (SQLException e) {
 			e.printStackTrace();
-			return false;
 		}
 		return null;
 	}
---
+++ src/com/rest/resources/Metric1Response.java
@@ -0,0 +1,22 @@
+package com.rest.resources;
+
+public class Metric1Response {
+
+	private String url;
+	private Integer position;
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public Integer getPosition() {
+		return position;
+	}
+	public void setPosition(Integer position) {
+		this.position = position;
+	}
+	
+	
+	
+}
