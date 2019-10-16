99ca259c820b81fb2d5ee401d0fcab22119f7f14
mthiago@grupodimed.com.br <mthiago@grupodimed.com.br>
2019-10-16 08:53:36 -0300

    db config properties

--- src/br/com/java/database/DatabaseConnection.java
+++ src/br/com/java/database/DatabaseConnection.java
@@ -3,6 +3,7 @@ package br.com.java.database;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ResourceBundle;
 
 public class DatabaseConnection {
 
@@ -10,12 +11,16 @@ public class DatabaseConnection {
 	
 	private static final String DB_USER = "root";
 	private static final String DB_PASSWORD = "123456";
+	private static ResourceBundle bundle;
+	private static String caminho;
 	
 	//ok - possível melhoria url relativa dbPath
 	public static Connection getConnection() throws SQLException {
 		try {
+			bundle = ResourceBundle.getBundle("DatabaseConfig");
+			caminho = bundle.getString("caminho");
 			Class.forName("org.sqlite.JDBC");
-			String dbPath = "jdbc:sqlite:C:/Users/thiag/Desktop/projetos/log-acess-api/utils/database/database.db";
+			String dbPath = "jdbc:sqlite:" + caminho;
 			Connection conn = DriverManager.getConnection(dbPath, DB_USER, DB_PASSWORD);
 			return conn;
 		} catch (Exception e) {
